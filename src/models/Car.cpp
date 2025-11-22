6303f7914865b2c786984d16a921ee823212e688
Oleksandr SHYMKIV <sasha220607@gmail.com>
2025-11-22 19:46:58 +0200

    add toJSON() and fromJSON()

--- src/models/Car.cpp
+++ src/models/Car.cpp
@@ -16,3 +16,21 @@ void Car::setYear(const int& year) { this->year = year; }
 void Car::setPricePerDay(const double& pricePerDay) { this->pricePerDay = pricePerDay; }
 
 
+crow::json::wvalue Car::toJSON() const{
+    crow::json::wvalue json;
+    json["id"] = this->id;
+    json["brand"] = this->brand;
+    json["model"] = this->model;
+    json["year"] = this->year;
+    json["pricePerDay"] = this->pricePerDay;
+    return json;
+}
+
+void Car::fromJSON(const crow::json::rvalue& json) {
+
+    if (json.has("id")) this->id = json["id"].i();
+    if (json.has("brand")) this->brand = json["brand"].s();
+    if (json.has("model")) this->model = json["model"].s();
+    if (json.has("year")) this->year = json["year"].i();
+    if (json.has("pricePerDay")) this->pricePerDay = json["pricePerDay"].d();
+}
