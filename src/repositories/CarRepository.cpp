7c23c8c59c244afc5e1e60ecacb634ef075dbf57
Oleksandr SHYMKIV <sasha220607@gmail.com>
2025-11-23 12:04:18 +0200

    add findById()

--- src/repositories/CarRepository.cpp
+++ src/repositories/CarRepository.cpp
@@ -1,9 +1,21 @@
 #include "repositories/CarRepository.h"
 
+using namespace std;
+
 void CarRepository::addCar(const Car& car) {
     cars.push_back(car);
 }
 
-std::vector<Car> CarRepository::getAllCars() const {
+vector<Car> CarRepository::getAllCars() const {
     return cars;
 }
+
+Car CarRepository::findById(int& id){
+
+    for(auto car : cars){
+        if(car.getId() == id) return car;
+    }
+
+    return;
+}
+
