e614f237116c4ff8e289cf1f6763acd172e76f7e
Oleksandr SHYMKIV <sasha220607@gmail.com>
2025-11-23 11:44:13 +0200

    add using namespace std

--- include/models/Car.h
+++ include/models/Car.h
@@ -2,26 +2,28 @@
 #include <string>
 #include <crow.h>
 
+using namespace std;
+
 class Car {
 private:
     int id;
-    std::string brand;
-    std::string model;
+    string brand;
+    string model;
     int year;
     double pricePerDay;
 
 public:
-    Car(int id, std::string brand, std::string model, int year, double pricePerDay);
+    Car(int id, string brand, string model, int year, double pricePerDay);
 
     int getId() const;
-    std::string getBrand() const;
-    std::string getModel() const;
+    string getBrand() const;
+    string getModel() const;
     int getYear() const;
     double getPricePerDay() const;
 
     void setId(const int&);
-    void setBrand(const std::string&);
-    void setModel(const std::string&);
+    void setBrand(const string&);
+    void setModel(const string&);
     void setYear(const int&);
     void setPricePerDay(const double&);
 
--- src/models/Car.cpp
+++ src/models/Car.cpp
@@ -1,17 +1,20 @@
 #include "Car.h"
+#include <string>
 
-Car::Car(int id, std::string brand, std::string model, int year, double pricePerDay)
+using namespace std;
+
+Car::Car(int id, string brand, string model, int year, double pricePerDay)
     : id(id), brand(brand), model(model), year(year), pricePerDay(pricePerDay) {}
 
 int Car::getId() const { return id; }
-std::string Car::getBrand() const { return brand; }
-std::string Car::getModel() const { return model; }
+string Car::getBrand() const { return brand; }
+string Car::getModel() const { return model; }
 int Car::getYear() const { return year; }
 double Car::getPricePerDay() const { return pricePerDay; }
 
 void Car::setId(const int& id) { this->id = id; }
-void Car::setBrand(const std::string& brand) { this->brand = brand; }
-void Car::setModel(const std::string& model) { this->model = model; }
+void Car::setBrand(const string& brand) { this->brand = brand; }
+void Car::setModel(const string& model) { this->model = model; }
 void Car::setYear(const int& year) { this->year = year; }
 void Car::setPricePerDay(const double& pricePerDay) { this->pricePerDay = pricePerDay; }
 
@@ -34,5 +37,5 @@ void Car::fromJSON(const crow::json::rvalue& json) {
         this->model = json["model"].s();
         this->year = json["year"].i();
         this->pricePerDay = json["pricePerDay"].d();
-    } catch (const std::exception& e) {}
+    } catch (const exception& e) {}
 }
